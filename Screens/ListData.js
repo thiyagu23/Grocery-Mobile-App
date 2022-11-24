f4803f1f51baa140f9881346eb03ef1b39d39ddc
Thiyagu M <trendythiyagu1523@gmail.com>
2022-11-24 23:09:15 +0530

    data adding

--- Screens/ListData.js
+++ Screens/ListData.js
@@ -1,5 +1,67 @@
 const Data = [
   {
     id: 1,
+    name: "apple",
+    img: require("./Data-imgs/apple.png"),
+  },
+  {
+    id: 2,
+    name: "beetroot",
+    img: require("./Data-imgs/beetroot.png"),
+  },
+  {
+    id: 3,
+    name: "brinjal",
+    img: require("./Data-imgs/brinjal.png"),
+  },
+  {
+    id: 4,
+    name: "carrot",
+    img: require("./Data-imgs/carrot.png"),
+  },
+  {
+    id: 5,
+    name: "fruits",
+    img: require("./Data-imgs/fruits.png"),
+  },
+  {
+    id: 6,
+    name: "grapes",
+    img: require("./Data-imgs/grapes.png"),
+  },
+  {
+    id: 7,
+    name: "green-chilli",
+    img: require("./Data-imgs/green-chilli.png"),
+  },
+  {
+    id: 8,
+    name: "lily-plant",
+    img: require("./Data-imgs/lily-plant.png"),
+  },
+  {
+    id: 9,
+    name: "orange",
+    img: require("./Data-imgs/orange.png"),
+  },
+  {
+    id: 10,
+    name: "potato",
+    img: require("./Data-imgs/potato.png"),
+  },
+  {
+    id: 11,
+    name: "pumpkin",
+    img: require("./Data-imgs/pumpkin.png"),
+  },
+  {
+    id: 12,
+    name: "spinach",
+    img: require("./Data-imgs/spinach.png"),
+  },
+  {
+    id: 13,
+    name: "tomatos",
+    img: require("./Data-imgs/tomatos.png"),
   },
 ];
